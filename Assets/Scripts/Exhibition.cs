b9d9516d28c4b1876a53a8e09fe18c8cee18a193
GO <go05sato@gmail.com>
2016-12-18 03:51:28 +0900

    サンプルのモデルとシーン追加

---
+++ Assets/Scripts/Exhibition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exhibition : MonoBehaviour {
+
+    [SerializeField]
+    private float _speed = 1;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, Time.deltaTime * _speed, 0),Space.World);
+    }
+}
